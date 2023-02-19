37d70cef92622487112868b917355e901327a64f
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-19 22:53:24 +0000

    Added the next test to get passing.

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,3 +1,4 @@
+require 'date'
 class BankAccount
   def initialize(starting_balance:)
     @initial_balance = 0
@@ -17,7 +18,18 @@ describe 'Bank Account' do
     expect(statement).to eq(expected_statement)
   end
 
-  example 'opening a bank account with an initial deposit'
+  example 'opening a bank account with an initial deposit' do
+    pending('TODO')
+    bank_account = BankAccount.new(starting_balance: 20)
+
+    statement = bank_account.print_statement
+
+    expected_statement = [
+      'Date Amount Balance',
+      "#{Date.today.strftime('%d.%m.%Y')} 20 20"
+    ].join("\n")
+    expect(statement).to eq(expected_statement)
+  end
 
   example 'cannot open a bank account with a negative initial deposit'
 end
