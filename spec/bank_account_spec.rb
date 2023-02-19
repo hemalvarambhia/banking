66dcc04be85f13e0cf7f95db3a5fecc91af1c88a
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-19 22:09:53 +0000

    Added a test list for opening a bank account.

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,7 @@
+describe 'Bank Account' do
+  example 'opening a bank account with zero balance'
+
+  example 'opening a bank account with an initial deposit'
+
+  example 'cannot open a bank account with a negative initial deposit'
+end
