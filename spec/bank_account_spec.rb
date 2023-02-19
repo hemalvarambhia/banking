715230cb7918ae74ed2a88caf00dfbe2626dea7d
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-19 22:36:01 +0000

    We can now print the statement for a new opened account with no starting balance or transactions.

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,10 +1,20 @@
-BankAccount = Class.new
+class BankAccount
+  def initialize(starting_balance:)
+    @initial_balance = 0
+  end
+
+  def print_statement
+    'Date Amount Balance'
+  end
+end
 describe 'Bank Account' do
   example 'opening a bank account with zero balance' do
-    pending('TODO')
     bank_account = BankAccount.new(starting_balance: 0)
-    expected_statement = ['Date Amount Balance', "#{Date.today} 0 0"].join("\n")
-    expect(bank_account.print_statement).to eq(expected_statement)
+
+    statement = bank_account.print_statement
+
+    expected_statement = 'Date Amount Balance'
+    expect(statement).to eq(expected_statement)
   end
 
   example 'opening a bank account with an initial deposit'
