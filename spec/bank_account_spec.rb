d90f64aef7b1a771214418c3f33ac9d46e366c16
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-19 22:19:49 +0000

    Added the next test to get passing.

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,10 @@
 describe 'Bank Account' do
-  example 'opening a bank account with zero balance'
+  example 'opening a bank account with zero balance' do
+    pending('TODO')
+    bank_account = BankAccount.new(starting_balance: 0)
+    expected_statement = ['Date Amount Balance', "#{Date.today} 0 0"].join("\n")
+    expect(bank_account.print_statement).to eq(expected_statement)
+  end
 
   example 'opening a bank account with an initial deposit'
 
