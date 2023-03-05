fe93798467d5517e345884092c69eef1a5e10656
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-05 11:44:01 +0000

    Added a test list that describes depositing money into an account.

---
+++ spec/depositing_into_an_account_spec.rb
@@ -0,0 +1,9 @@
+describe 'Bank Account' do
+  describe 'depositing into an account' do
+    it 'does not increase the balance on the account with nothing is deposited'
+
+    it 'increases the balance on the account when a non-zero amount is deposited'
+
+    it 'does not allow a negative amount to be deposited into the account'
+  end
+end
