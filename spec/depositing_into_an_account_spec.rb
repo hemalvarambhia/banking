18e9c0a6a0b0822d4580bb61a1b0b55903ac18d8
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-05 14:31:47 +0000

    We can now deposit nothing into the bank account.

--- spec/depositing_into_an_account_spec.rb
+++ spec/depositing_into_an_account_spec.rb
@@ -1,6 +1,53 @@
+require 'date'
+require 'ostruct'
+class Transaction
+  def initialize(date:, amount:)
+    @date = date
+    @amount = amount
+  end
+
+  def to_s
+    "#{@date.strftime('%d.%m.%Y')} #{@amount}"
+  end
+end
+BankAccountSetUpIncorrectly = Class.new(StandardError)
+class BankAccount
+  def initialize(initial_deposit:)
+    raise BankAccountSetUpIncorrectly.new('Cannot open with a negative deposit') if initial_deposit < 0
+    @initial_balance = initial_deposit
+    @transaction = Transaction.new(date: Date.today, amount: initial_deposit)
+  end
+
+  def deposit(amount)
+
+  end
+
+  def current_balance
+    @initial_balance
+  end
+
+  def print_statement
+    column_titles = 'Date Amount Balance'
+    if @initial_balance.zero?
+      column_titles
+    else
+      [
+        column_titles,
+        "#{@transaction.to_s} #{@initial_balance}"
+      ].join("\n")
+    end
+  end
+end
+
 describe 'Bank Account' do
   describe 'depositing into an account' do
-    it 'does not increase the balance on the account with nothing is deposited'
+    it 'does not increase the balance on the account with nothing is deposited' do
+      account = BankAccount.new(initial_deposit: 50)
+
+      account.deposit(0)
+
+      expect(account.current_balance).to eq(50)
+    end
 
     it 'increases the balance on the account when a non-zero amount is deposited'
 
