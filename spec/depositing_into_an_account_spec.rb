eda83fc1991d520a94257fb38a2c7154eb42101e
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-05 14:34:12 +0000

    Copied duplicate code (divergent change) to a more sensible place.

--- spec/depositing_into_an_account_spec.rb
+++ spec/depositing_into_an_account_spec.rb
@@ -1,43 +1,6 @@
 require 'date'
 require 'ostruct'
-class Transaction
-  def initialize(date:, amount:)
-    @date = date
-    @amount = amount
-  end
-
-  def to_s
-    "#{@date.strftime('%d.%m.%Y')} #{@amount}"
-  end
-end
-BankAccountSetUpIncorrectly = Class.new(StandardError)
-class BankAccount
-  def initialize(initial_deposit:)
-    raise BankAccountSetUpIncorrectly.new('Cannot open with a negative deposit') if initial_deposit < 0
-    @initial_balance = initial_deposit
-    @transaction = Transaction.new(date: Date.today, amount: initial_deposit)
-  end
-
-  def deposit(amount)
-
-  end
-
-  def current_balance
-    @initial_balance
-  end
-
-  def print_statement
-    column_titles = 'Date Amount Balance'
-    if @initial_balance.zero?
-      column_titles
-    else
-      [
-        column_titles,
-        "#{@transaction.to_s} #{@initial_balance}"
-      ].join("\n")
-    end
-  end
-end
+require_relative '../lib/bank_account'
 
 describe 'Bank Account' do
   describe 'depositing into an account' do
