1e4d52f45e7c5d8f6fd27a1cae9061ffcbf383ff
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-11 15:18:29 +0000

    Added the next test to get passing.

--- spec/depositing_into_an_account_spec.rb
+++ spec/depositing_into_an_account_spec.rb
@@ -35,6 +35,11 @@ describe 'Bank Account' do
       expect(account.print_statement).to eq(expected_statement)
     end
 
-    it 'does not allow a negative amount to be deposited into the account'
+    it 'does not allow a negative amount to be deposited into the account' do
+      pending('TODO')
+      account = BankAccount.new(initial_deposit: 0)
+
+      expect { account.deposit(-50) }.to raise_error('Cannot deposit 50 robux')
+    end
   end
 end
