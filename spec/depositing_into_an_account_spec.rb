6bbf86a7e5687e7bce73c082d3a0364cf1857752
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-11 16:05:22 +0000

    Test passes: an exception when a negative amount is deposited.

--- spec/depositing_into_an_account_spec.rb
+++ spec/depositing_into_an_account_spec.rb
@@ -36,10 +36,9 @@ describe 'Bank Account' do
     end
 
     it 'does not allow a negative amount to be deposited into the account' do
-      pending('TODO')
       account = BankAccount.new(initial_deposit: 0)
 
-      expect { account.deposit(-50) }.to raise_error('Cannot deposit 50 robux')
+      expect { account.deposit(-50) }.to raise_error('Cannot deposit a negative amount')
     end
   end
 end
