dc2ea51774fabdecd551be6d438a8a3245bead2d
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-12 14:03:59 +0000

    A test list for withdrawing from a bank account

---
+++ spec/withdrawing_from_a_bank_account_spec.rb
@@ -0,0 +1,9 @@
+describe 'Withdrawing from a bank account' do
+  example 'withdrawing nothing from a bank account'
+
+  example 'withdrawing an amount from a bank account'
+
+  example 'withdrawing a negative amount from a bank account'
+
+  example 'becoming overdrawn'
+end
