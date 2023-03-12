0d5f942362e761b19b6379acc1164f432e7e576b
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-12 20:25:52 +0000

    We can withdraw an amount of money from a bank account.

--- spec/withdrawing_from_a_bank_account_spec.rb
+++ spec/withdrawing_from_a_bank_account_spec.rb
@@ -7,7 +7,14 @@ describe 'Withdrawing from a bank account' do
     expect(account.current_balance).to eq(45)
   end
 
-  example 'withdrawing an amount from a bank account'
+  example 'withdrawing an amount from a bank account' do
+    account = BankAccount.new(initial_deposit: 0)
+
+    account.deposit(100)
+    account.withdraw(50)
+
+    expect(account.current_balance).to eq(50)
+  end
 
   example 'withdrawing a negative amount from a bank account'
 
