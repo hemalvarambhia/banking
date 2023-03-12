bef874b1f635e8005ac7d041bf80ec145a63977b
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-12 20:33:15 +0000

    Added the next test to get passing.

--- spec/withdrawing_from_a_bank_account_spec.rb
+++ spec/withdrawing_from_a_bank_account_spec.rb
@@ -16,7 +16,12 @@ describe 'Withdrawing from a bank account' do
     expect(account.current_balance).to eq(50)
   end
 
-  example 'withdrawing a negative amount from a bank account'
+  example 'withdrawing a negative amount from a bank account' do
+    pending('TODO')
+    account = BankAccount.new(initial_deposit: 0)
+
+    expect { account.withdraw(-131) }.to raise_error('Cannot withdraw a negative amount of money')
+  end
 
   example 'becoming overdrawn'
 
