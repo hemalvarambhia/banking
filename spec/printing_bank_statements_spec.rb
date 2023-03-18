80f38ebfb25ff24edc8870362f815049b2fb7d39
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-18 16:57:37 +0000

    Added a test list for printing bank statements.

---
+++ spec/printing_bank_statements_spec.rb
@@ -0,0 +1,13 @@
+describe 'Printing bank statements' do
+  it 'prints the bank statement when only one deposit is made'
+
+  it 'prints the bank statement when only one withdrawal is made'
+
+  it 'prints the bank statement when no transactions were made'
+
+  it 'prints the bank statement when two deposits are made'
+
+  it 'prints the bank statement when two withdrawals are made'
+
+  it 'prints the bank statements when a deposit is followed by a withdrawal'
+end
