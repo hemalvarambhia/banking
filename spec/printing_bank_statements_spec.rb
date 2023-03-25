b1e2f4ec0b5fe42042b7ccc3f7fc5eb6a2acd2a6
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-25 17:07:54 +0000

    Made the descriptions concise.

--- spec/printing_bank_statements_spec.rb
+++ spec/printing_bank_statements_spec.rb
@@ -1,13 +1,13 @@
 describe 'Printing bank statements' do
-  it 'prints the bank statement when only one deposit is made'
+  example 'when only one deposit is made'
 
-  it 'prints the bank statement when only one withdrawal is made'
+  it 'when only one withdrawal is made'
 
-  it 'prints the bank statement when no transactions were made'
+  it 'when no transactions were made'
 
-  it 'prints the bank statement when two deposits are made'
+  it 'when two deposits are made'
 
-  it 'prints the bank statement when two withdrawals are made'
+  it 'when two withdrawals are made'
 
-  it 'prints the bank statements when a deposit is followed by a withdrawal'
+  it 'when a deposit is followed by a withdrawal'
 end
