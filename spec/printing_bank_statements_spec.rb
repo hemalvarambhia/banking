322200a5873d0b5846200898682abffda785ef32
hemalvarambhia <h.n.varambhia@gmail.com>
2023-03-26 20:18:39 +0100

    We can print a bank statement with one deposit.

--- spec/printing_bank_statements_spec.rb
+++ spec/printing_bank_statements_spec.rb
@@ -1,5 +1,17 @@
 describe 'Printing bank statements' do
-  example 'when only one deposit is made'
+  example 'when only one deposit is made' do
+    bank_account = BankAccount.new(initial_deposit: 0, overdraft_limit: 2000)
+    bank_account.deposit(50)
+
+    statement = bank_account.print_statement
+
+    with_one_deposit =
+      [
+        'Date Amount Balance',
+        "#{Date.today.strftime('%d.%m.%Y')} 50 50"
+      ].join("\n")
+    expect(statement).to eq(with_one_deposit)
+  end
 
   example 'when only one withdrawal is made'
 
