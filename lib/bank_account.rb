3f3e51691443ed612128597f27239b6aef4240ee
hemalvarambhia <h.n.varambhia@gmail.com>
2023-04-05 20:41:15 +0100

    Test passes: simplified the code by removing the `to_enum`.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -27,7 +27,7 @@ class BankAccount
   end
 
   def statement_lines
-    @transactions.to_enum.with_index.map do |transaction, number|
+    @transactions.map.with_index do |transaction, number|
       print_transaction(transaction) + " " + "#{running_total_up_to(number)}"
     end.join("\n")
   end
