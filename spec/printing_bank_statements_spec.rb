4fe3c3a53994240d88524a1e120b8b4ce93213f5
hemalvarambhia <h.n.varambhia@gmail.com>
2023-04-16 13:34:17 +0100

    Test passes: replaced the presentation method with the domain one.

--- spec/printing_bank_statements_spec.rb
+++ spec/printing_bank_statements_spec.rb
@@ -3,21 +3,18 @@ describe 'Printing bank statements' do
     example 'when no transactions were made' do
       bank_account = BankAccount.new(initial_deposit: 0, overdraft_limit: -2000)
 
-      transaction_lines = bank_account.statement_lines
+      statement_lines = bank_account.bank_statement_lines
 
-      with_no_deposits = ""
-      expect(transaction_lines).to eq(with_no_deposits)
+      expect(statement_lines).to be_empty
     end
 
     example 'a single deposit transaction' do
       bank_account = BankAccount.new(initial_deposit: 0, overdraft_limit: -2000)
       bank_account.deposit(5)
 
-      transaction_lines = bank_account.statement_lines
+      statement_lines = bank_account.bank_statement_lines
 
-      with_two_deposits = "#{Date.today.strftime('%d.%m.%Y')} 5 5"
-
-      expect(transaction_lines).to eq(with_two_deposits)
+      expect(statement_lines).to eq(["#{Date.today.strftime('%d.%m.%Y')} 5 5"])
     end
 
     example 'two deposit transactions' do
@@ -25,11 +22,13 @@ describe 'Printing bank statements' do
       bank_account.deposit(5)
       bank_account.deposit(5)
 
-      transaction_lines = bank_account.statement_lines
+      statement_lines = bank_account.bank_statement_lines
 
-      with_two_deposits = "#{Date.today.strftime('%d.%m.%Y')} 5 5\n#{Date.today.strftime('%d.%m.%Y')} 5 10"
-
-      expect(transaction_lines).to eq(with_two_deposits)
+      expected = [
+        "#{Date.today.strftime('%d.%m.%Y')} 5 5",
+        "#{Date.today.strftime('%d.%m.%Y')} 5 10"
+      ]
+      expect(statement_lines).to eq(expected)
     end
   end
 
