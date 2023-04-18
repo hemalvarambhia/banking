5b39a1a641acb1bc5dc983af2b924fcd6c20d447
hemalvarambhia <h.n.varambhia@gmail.com>
2023-04-18 23:08:27 +0100

    Extracted computation to an intention-revealing method.

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -10,11 +10,15 @@ class BankStatement
 
   def lines
     @bank_account.map.with_index do |transaction, number|
-      print_transaction(transaction) + " " + "#{@bank_account.running_total_up_to(number)}"
+      print_transaction(transaction) + " " + "#{balance_as_of(number)}"
     end
   end
 
   private
+
+  def balance_as_of(number)
+    @bank_account.running_total_up_to(number)
+  end
 
   def print_transaction(transaction)
     "#{transaction.date.strftime('%d.%m.%Y')} #{transaction.amount}"
