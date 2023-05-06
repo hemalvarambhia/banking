8701436af1154bcacab4a3a66fb163b0a27f0f93
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-06 20:13:12 +0100

    Added more tests to the list.

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -4,5 +4,13 @@ describe 'Money' do
     expect(Money.new(5.00)).to be < 6.51
   end
 
-  example 'we can add two amounts of money'
+  example 'we can add two positive amounts of money'
+
+  example 'we can add a positive amount of money to a negative one and get a positive total'
+
+  example 'we can add a positive amount of money to a negative one and get a negative total'
+
+  example 'we can add a positive amount of money to a negative one and compute a zero total'
+
+  example 'we can add two negative amounts of money'
 end
