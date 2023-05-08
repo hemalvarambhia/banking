0598bd547e8d3e8bb61adf09340a1d2b5a116a94
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-08 20:54:34 +0100

    Made the behaviour being tested clearer.

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/money'
 describe 'Money' do
-  example 'an amount of money being less than another' do
+  example 'an amount of money being less than another number' do
     expect(Money.new(5.00)).to be < 6.51
   end
 
