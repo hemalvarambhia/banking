a66b332ed3f702fb03d6602995aa0fcfe9224fd7
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-11 17:31:18 +0100

    Grouped the tests for adding money into a describe block.

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -10,33 +10,35 @@ describe 'Money' do
     end
   end
 
-  example 'we can add two positive amounts of money' do
-    total = Money.new(4.50) + Money.new(6.00)
+  describe 'adding money' do
+    example 'we can add two positive amounts of money' do
+      total = Money.new(4.50) + Money.new(6.00)
 
-    expect(total.value).to eq(10.50)
-  end
+      expect(total.value).to eq(10.50)
+    end
 
-  example 'we can add a positive amount of money to a negative one and get a positive total' do
-    total = Money.new(6.50) + Money.new(-1.10)
+    example 'we can add a positive amount of money to a negative one and get a positive total' do
+      total = Money.new(6.50) + Money.new(-1.10)
 
-    expect(total.value).to eq(5.40)
-  end
+      expect(total.value).to eq(5.40)
+    end
 
-  example 'we can add a positive amount of money to a negative one and get a negative total' do
-    total = Money.new(10.00) + Money.new(-11.99)
+    example 'we can add a positive amount of money to a negative one and get a negative total' do
+      total = Money.new(10.00) + Money.new(-11.99)
 
-    expect(total.value).to eq(-1.99)
-  end
+      expect(total.value).to eq(-1.99)
+    end
 
-  example 'we can add a positive amount of money to a negative one and compute a zero total' do
-    total = Money.new(19.11) + Money.new(-19.11)
+    example 'we can add a positive amount of money to a negative one and compute a zero total' do
+      total = Money.new(19.11) + Money.new(-19.11)
 
-    expect(total.value).to eq(0.00)
-  end
+      expect(total.value).to eq(0.00)
+    end
 
-  example 'we can add two negative amounts of money' do
-    total = Money.new(-5.11) + Money.new(-0.02)
+    example 'we can add two negative amounts of money' do
+      total = Money.new(-5.11) + Money.new(-0.02)
 
-    expect(total.value).to eq(-5.13)
+      expect(total.value).to eq(-5.13)
+    end
   end
 end
